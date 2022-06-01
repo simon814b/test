c58a1e90e947e03bf097dd0cbc4be420b8deab94
simon814b <simonbihoreau@gmail.com>
2022-06-01 19:35:58 +0200

    listen on 8080

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const http = require('http');
+
+const hostname = '';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 const main = async () => {
 	console.log("Hello you");
 }
